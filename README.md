c87e5e158c075089bbeaf6f1e29a31754aeecf7e
Jeorge Donato <jeorgekhenr@gmail.com>
2020-06-22 14:16:47 -0700

    Added some validations inside the functions and styled the alert and clipboard button

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 <h2 align="center">Password Generator</h2>
+
+**Working Link:** https://jeorgedonato.github.io/password-generator/
+
+<p align="center">A dynamic website utilizing Javascript's native functions.</p>
+
+**Technologies Used:**
+- **HTML**
+- **CSS**
+- **Javascript**
+
+****
+**Screenshots**
+
+![Home Screenshot](/assets/home-screenshot.png)
+
+![Validation Screenshot](/assets/validation-screenshot.png)
+
+![Function Screenshot](/assets/function-screenshot.png)
+
+![Password Screenshot](/assets/password-screenshot.png)
+****
+
+<h5 align="center">© 2020 Jeorge Janil Donato</h5>
--- script.js
+++ script.js
@@ -13,25 +13,27 @@ let generatePassword = butId => {
   //Prompting the password length and validation
   let passInput = '';
   let pLen = prompt("Password Length should be minimum of 8 and maximum of 128 characters.");
+  //Constructing object instead of array for better readability
   let passwordCriteria = {
     'uppercase': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
     'lowercase': 'abcdefghijklmnopqrstuvwxyz',
     'numbers': '0123456789',
     'special': `@%+'/!#$^?:;.(){}[]~`
   };
-
+  //Validating if string is convertible to int and if password is between 8-128 characters
   if (!isNaN(pLen) && parseInt(pLen) >= 8 && parseInt(pLen) <= 128) {
     let isLowerCase = confirm("Does it include lowercase characters?");
     let isUpperCase = confirm("Does it include uppercase characters?");
     let isNumeric = confirm("Does it include numeric characters?");
     let isSpecial = confirm("Does it include special characters?");
 
+    //Checking the user's criteria to be added in string
     let passwordChar = isLowerCase ? passwordCriteria['lowercase'] : '';
     passwordChar += isUpperCase ? passwordCriteria['uppercase'] : '';
     passwordChar += isNumeric ? passwordCriteria['numbers'] : '';
     passwordChar += isSpecial ? passwordCriteria['special'] : '';
-    // console.log(passwordChar)
-    // if()
+
+    //Validating user's criteria if they chose at least one criteria 
     if (passwordChar !== '') {
       for (let i = 1; i <= parseInt(pLen); i++) {
         let char = Math.floor(Math.random()
@@ -40,6 +42,7 @@ let generatePassword = butId => {
         passInput += passwordChar.charAt(char);
       }
       // console.log(passInput)
+      document.getElementById("span-button").style.display = 'block';
       return passInput;
       // `${passInput} Password Length : ${parseInt(pLen)} Characters
       //                         Lowercase : ${boolTranslate(isLowerCase)} 
@@ -47,16 +50,39 @@ let generatePassword = butId => {
       //                         Numeric : ${boolTranslate(isNumeric)} 
       //                         Special Characters : ${isSpecial}`;
     } else {
-      alert('Please select at least one criteria');
+      alert('Please select at least ONE Criteria');
+      document.getElementById("span-button").style.display = 'none';
       return "Your secure password";
     }
 
   } else {
     alert("Password Characters should be between 8 - 128 characters.");
+    document.getElementById("span-button").style.display = 'none';
     return "Your Secure Password";
   }
 }
 
+//Copy to clipboard function
+let copyToClip = () => {
+  let copyText = document.getElementById("password");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  // alert("Copied the text: " + copyText.value);
+  document.getElementById("alert-clip").style.display = 'block';
+
+  //Setting time out to hide alert
+  setTimeout(function () { document.getElementById("alert-clip").style.display = "none"; }, 600);
+}
+
+
+//Translate bool to string Yes for true and No for false
 let boolTranslate = bool => bool ? 'Yes' : 'No';
 
 // Add event listener to generate button
